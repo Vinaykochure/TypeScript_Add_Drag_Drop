b3ba69d9b9c4770b1cd15ac43497296e24c0292e
07-vinay <vinaykochure@gmail.com>
2023-02-11 23:31:05 +0530

    working with es5 module

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,6 @@
-//Below are import statement in namespace(///)
-/*
-/// <reference path="models/drag-drop.ts"/>
-///<reference path="models/project.ts"/>
-///<reference path="state/project-state.ts"/>
-///<reference path="util/validators.ts"/>
-///<reference path="decorators/autobind.ts"/>
+import { ProjectInput } from './components/project-input.js';
+import { ProjectList } from './components/project-list.js';
 
-///<reference path="components/base-component.ts"/>
-*/
-
-///<reference path="components/project-input.ts"/>
-///<reference path="components/project-list.ts"/>
-
-namespace App {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // autobind decorater
-  export function autobind(
-    _target: any,
-    _methodName: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const orignalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = orignalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// autobind decorater
+export function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const orignalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = orignalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
